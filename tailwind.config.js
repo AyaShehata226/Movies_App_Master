e5f06f0160ca4681e45174a618dcd345ea635e86
AyaShehata226 <ayashegata@gmail.com>
2025-02-17 10:18:33 +0200

    app

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'main-color': '#ff2c1f',
+        'text-color': '#020307',
+        'bg-color': '#fff',
+      },
+      screens:{
+        'custom-lg':'1080px',
+        'custom-xmd':'991px',
+        'custom-md':'774px',
+        'custom-xsm':'472px',
+        'custom-sm':'370px',
+        'custom-xxs':'300px',
+      }
+    },
+  },
+  plugins: [],
+}
